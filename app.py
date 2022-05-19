420e5ac08ba26ed276bae426c561bdb172d852a9
Kalyn Bolton <kalynbolton@yahoo.com>
2022-05-18 20:20:19 -0500

    Part of Lesson 5 push

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+# import dependencies for sqlALCHEMY
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# import flask dependencies
+from flask import Flask, jsonify
+
+# set up database
+engine = create_engine("sqlite:///hawaii.sqlite")
